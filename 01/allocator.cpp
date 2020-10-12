8a9f8a795a10739778822ae6179927f1f706cb58
zladimir <zolodya@mail.ru>
2020-10-12 10:25:47 +0300

    completed5

--- 01/allocator.cpp
+++ 01/allocator.cpp
@@ -3,8 +3,8 @@
 
 Allocator::Allocator()
 {
-	max_size_ = static_cast<size_t>(0);
-	available_size_ = static_cast<size_t>(0);
+	max_size_ = 0;
+	available_size_ = 0;
 	memory_begin_ = nullptr;
 	offset_ = nullptr;
 }
@@ -14,13 +14,12 @@ void Allocator::makeAllocator(size_t maxSize)
 	if (memory_begin_ == nullptr) {
 		try {
 			memory_begin_ = new char[maxSize];
-			std::cout << "ANSWER: \tAllocator of " << maxSize << " bytes was created" << std::endl;
 			max_size_ = maxSize;
 			available_size_ = maxSize;
 			offset_ = memory_begin_;
 		} 
-		catch(...) {
-			std::cout << "ERROR:  \tFailed to allocate memory" << std::endl;
+		catch(std::bad_alloc &e) {
+			std::cout << "ERROR:  \t" << e.what() << std::endl;
 		}
 	} else {
 		std::cout << "WARNING:\tYou can't change the size of the allocator" << std::endl;
@@ -49,5 +48,4 @@ void Allocator::reset()
 Allocator::~Allocator()
 {
 	delete[] memory_begin_;
-	std::cout << "ANSWER: \tAllocator is destroyed" << std::endl;
 }
--- 01/test.cpp
+++ 01/test.cpp
@@ -4,149 +4,104 @@
 bool check(char *p)
 {
 	if (p == nullptr){
-		std::cout << "-\nWARNING:\tYou can not allocate that much memory" << std::endl;
+		std::cout << "WARNING:\tYou can not allocate that much memory" << std::endl;
 		return 1;
 	} else {
-		std::cout << static_cast<void *>(p) << std::endl;
 		return 0;
 	}
 }
 
-void alloc_command_printer(char *p, size_t num_bytes)
+void test_1()
 {
-	std::cout << "COMMAND:\talloc(" << num_bytes << ")\nANSWER: \tpointer to begin of memory: ";
-	check(p);
-}
-
-void test_1(size_t size)
-{
-	std::cout << "==========================================================" << std::endl;
-	std::cout << "TEST:\t\tEasy case" << std::endl;
+	std::cout << "Test 1" << std::endl;
 	char *ptr;
 	Allocator a;
-	std::cout << "COMMAND:\tmakeAllocator(" << size << ")" << std::endl;
-	a.makeAllocator(size);
-	ptr = a.alloc(size);
-	alloc_command_printer(ptr, size);
+	a.makeAllocator(100);
+	ptr = a.alloc(100);
+	check(ptr);
 }
 
-void test_2(size_t size)
+void test_2()
 {
-	std::cout << "==========================================================" << std::endl;
-	std::cout << "TEST:\t\tA few of the alloc" << std::endl;
+	std::cout << "Test 2" << std::endl;
 	char *ptr;
-	int num_bytes;
 	Allocator a;
-	std::cout << "COMMAND:\tmakeAllocator(" << size << ")" << std::endl;
-	a.makeAllocator(size);
-	num_bytes = 3;
-	ptr = a.alloc(num_bytes);
-	alloc_command_printer(ptr, num_bytes);
-	num_bytes = 2;
-	ptr = a.alloc(num_bytes);
-	alloc_command_printer(ptr, num_bytes);
-	num_bytes = 5;
-	ptr = a.alloc(num_bytes);
-	alloc_command_printer(ptr, num_bytes);
+	a.makeAllocator(10);
+	ptr = a.alloc(3);
+	check(ptr);
+	ptr = a.alloc(2);
+	check(ptr);
+	ptr = a.alloc(5);
+	check(ptr);
 }
 
-void test_3(size_t size)
+void test_3()
 {
-	std::cout << "==========================================================" << std::endl;
-	std::cout << "TEST:\t\tA few of the alloc with reset()" << std::endl;
+	std::cout << "Test 3" << std::endl;
 	char *ptr;
-	int num_bytes;
 	Allocator a;
-	std::cout << "COMMAND:\tmakeAllocator(" << size << ")" << std::endl;
-	a.makeAllocator(size);
-	num_bytes = 3;
-	ptr = a.alloc(num_bytes);
-	alloc_command_printer(ptr, num_bytes);
-	num_bytes = 2;
-	ptr = a.alloc(num_bytes);
-	alloc_command_printer(ptr, num_bytes);
-	std::cout << "COMMAND:\treset()" << std::endl;
+	a.makeAllocator(5);
+	ptr = a.alloc(3);
+	check(ptr);
+	ptr = a.alloc(2);
+	check(ptr);
 	a.reset();
-	num_bytes = 2;
-	ptr = a.alloc(num_bytes);
-	alloc_command_printer(ptr, num_bytes);
-	num_bytes = 1;
-	ptr = a.alloc(num_bytes);
-	alloc_command_printer(ptr, num_bytes);
-	num_bytes = 2;
-	ptr = a.alloc(num_bytes);
-	alloc_command_printer(ptr, num_bytes);
+	ptr = a.alloc(2);
+	check(ptr);
+	ptr = a.alloc(1);
+	check(ptr);
+	ptr = a.alloc(2);
+	check(ptr);
 }
 
-void test_4(size_t size)
+void test_4()
 {
-	std::cout << "==========================================================" << std::endl;
-	std::cout << "TEST:\t\tNot enough memory for alloc" << std::endl;
+	std::cout << "Test 4" << std::endl;
 	char *ptr;
-	int num_bytes;
 	Allocator a;
-	std::cout << "COMMAND:\tmakeAllocator(" << size << ")" << std::endl;
-	a.makeAllocator(size);
-	num_bytes = 60;
-	ptr = a.alloc(num_bytes);
-	alloc_command_printer(ptr, num_bytes);
-	num_bytes = 115;
-	ptr = a.alloc(num_bytes);
-	alloc_command_printer(ptr, num_bytes);
-	std::cout << "COMMAND:\treset()" << std::endl;
+	a.makeAllocator(100);
+	ptr = a.alloc(60);
+	check(ptr);
+	ptr = a.alloc(25);
+	check(ptr);
 	a.reset();
-	num_bytes = 45;
-	ptr = a.alloc(num_bytes);
-	alloc_command_printer(ptr, num_bytes);
-	num_bytes = 45;
-	ptr = a.alloc(num_bytes);
-	alloc_command_printer(ptr, num_bytes);
-	num_bytes = 50;
-	ptr = a.alloc(num_bytes);
-	alloc_command_printer(ptr, num_bytes);
-	num_bytes = 5;
-	ptr = a.alloc(num_bytes);
-	alloc_command_printer(ptr, num_bytes);
+	ptr = a.alloc(45);
+	check(ptr);
+	ptr = a.alloc(45);
+	check(ptr);
+	ptr = a.alloc(5);
+	check(ptr);
+	ptr = a.alloc(50);
+	check(ptr);
 }
 
-void test_5(size_t size)
+void test_5()
 {
-	std::cout << "==========================================================" << std::endl;
-	std::cout << "TEST:\t\tNot enough memory for initialization" << std::endl;
+	std::cout << "Test 5" << std::endl;
 	Allocator a;
-	std::cout << "COMMAND:\tmakeAllocator(" << size << ")" << std::endl;
-	a.makeAllocator(size);
+	a.makeAllocator(900000000000);
 }
 
-void test_6(size_t size)
+void test_6()
 {
-	std::cout << "==========================================================" << std::endl;
-	std::cout << "TEST:\t\tmakeAllocator change case" << std::endl;
+	std::cout << "Test 6" << std::endl;
 	char *ptr;
-	int num_bytes;
 	Allocator a;
-	std::cout << "COMMAND:\tmakeAllocator(" << size << ")" << std::endl;
-	a.makeAllocator(size);
-	num_bytes = 45;
-	ptr = a.alloc(num_bytes);
-	alloc_command_printer(ptr, num_bytes);
-	std::cout << "COMMAND:\tmakeAllocator(" << size << ")" << std::endl;
-	a.makeAllocator(size);
-	num_bytes = 25;
-	ptr = a.alloc(num_bytes);
-	alloc_command_printer(ptr, num_bytes);
-	num_bytes = 15;
-	ptr = a.alloc(num_bytes);
-	alloc_command_printer(ptr, num_bytes);
+	a.makeAllocator(60);
+	ptr = a.alloc(45);
+	check(ptr);
+	a.makeAllocator(120);
+	ptr = a.alloc(15);
+	check(ptr);
 }
 
 int main(int argc, char const *argv[])
 {
-	test_1(100);
-	test_2(10);
-	test_3(5);
-	test_4(100);
-	test_5(900000000000);
-	test_6(60);
+	test_1();
+	test_2();
+	test_3();
+	test_4();
+	test_5();
+	test_6();
 	return 0;
 }
